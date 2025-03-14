e01c4f831e976dd029b52e5221f73fe4627f1ced
GustavoFernandesSP <gustavo.fernandes@sptech.school>
2025-03-14 19:34:09 -0300

    atualizado!

--- index.js
+++ index.js
@@ -7,6 +7,10 @@ const PORT = 5000   // Indico a porta que irá rodar o serviço
 
 app.use(express.static(path.join(__dirname, "client")))   // Estou indicando o meu diretório estático client onde está minhas páginas http.
 
-app.listen(PORT, () => {
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "client", "index.html"));
+});
+
+app.listen(5000,"0.0.0.0" ,() => {
     console.log(`Servidor iniciado na porta:${PORT} - http://localhost:${PORT}/`);
 } )
